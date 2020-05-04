7f0f1e89b7272dc0dfa4f090b229c8d007f4fb02
Kirill Enykeev <enykeev@mech.sh>
2020-05-04 15:50:25 +0700

    nginx/prom/ab stack

--- mockserver.js
+++ mockserver.js
@@ -1,14 +1,27 @@
 const axios = require('axios')
 const express = require('express')
+const promMid = require('express-prometheus-middleware')
+const Prometheus = require('prom-client')
 
 const { PORT, WEBHOOK_PORT, BLACKHOLE_WEBHOOK } = process.env
 
+const webhookCounter = new Prometheus.Counter({
+  name: 'webhook_requests_total',
+  help: 'Counter for total webhooks sent',
+  labelNames: [],
+});
+
 const app = express()
+
+app.use(promMid({
+  metricsPath: '/metrics',
+  collectDefaultMetrics: true
+}))
 
 app.get('/:hostname/:port/:id', (req, res) => {
   const { hostname, port, id } = req.params
 
-  console.log(`http://${hostname}:${port}/${id}`)
+  webhookCounter.inc()
 
   axios.get(`http://${hostname}:${port}/${id}`)
     .catch(e => console.error('request failed:', e.toString(), id))
